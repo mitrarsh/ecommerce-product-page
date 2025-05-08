c42ce57428b284ccb135a4bac46e0609dc92a204
mitrarsh <mitraroshanfekr05@gmail.com>
2025-05-08 23:37:00 +0330

    full html

--- app.js
+++ app.js
@@ -1,77 +1,76 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
 
-// ////////// tab-bar
-// const tabBarLink = document.querySelectorAll(".tab-bar-link")
-// const underline = document.querySelector(".underline")
-// const iconMenu =document.querySelector(".icon-container-menu")
-// const sideBar =document.querySelector(".side-bar")
-// const closeBtn =document.querySelector(".close-btn")
-// const overlay =document.querySelector(".overlay")
-
-
-// tabBarLink.forEach((link=>{
-//     link.addEventListener("click",(e)=>{
-//     // ???????????????????????????
-//     })
-// }))
-
-// iconMenu.addEventListener("click", (e)=>{
-//     sideBar.classList.add("active");
-//     overlay.style.display="block";
-// })
-// closeBtn.addEventListener("click", (e)=>{
-//     sideBar.classList.remove("active");
-//     overlay.style.display="none";
-// })
+ ////////// tab-bar
+ const tabBarLink = document.querySelectorAll(".tab-bar-link")
+ const underline = document.querySelector(".underline")
+ const iconMenu =document.querySelector(".icon-container-menu")
+ const sideBar =document.querySelector(".side-bar")
+ const closeBtn =document.querySelector(".close-btn")
+const overlay =document.querySelector(".overlay")
 
 
-// //////////////card
-// const iconCard =document.querySelector(".icon-card")
-// const card =document.querySelector(".card")
+tabBarLink.forEach((link=>{
+    link.addEventListener("click",(e)=>{
+    // ???????????????????????????
+    })
+}))
 
-// iconCard.addEventListener("click", (e)=>{
-//     e.preventDefault();
-//     card.classList.toggle('active');
+iconMenu.addEventListener("click", (e)=>{
+    sideBar.classList.add("active");
+    overlay.style.display="block";
+})
+closeBtn.addEventListener("click", (e)=>{
+    sideBar.classList.remove("active");
+    overlay.style.display="none";
+})
 
-// })
+
+//////////////card
+const iconCard =document.querySelector(".icon-card")
+const card =document.querySelector(".card")
+
+iconCard.addEventListener("click", (e)=>{
+    e.preventDefault();
+    card.classList.toggle('active');
+
+})
 
 
-///////////////slider
-// document.addEventListener("DOMContentLoaded", () => {
-//     const nextBtn = document.querySelector("#nextBtn");
-//     const prevBtn = document.querySelector("#prevBtn");
-//     const slide = document.querySelectorAll(".slide");
-//     const slides = document.querySelector(".slides");
+/////////////slider
+document.addEventListener("DOMContentLoaded", () => {
+    const nextBtn = document.querySelector("#nextBtn");
+    const prevBtn = document.querySelector("#prevBtn");
+    const slide = document.querySelectorAll(".slide");
+    const slides = document.querySelector(".slides");
 
-//     let counter = 0;
+    let counter = 0;
 
-//     prevBtn.style.display = 'none';
+    prevBtn.style.display = 'none';
 
-//     nextBtn.addEventListener("click", () => {
-//         counter++;
-//         carousel();
-//     });
+    nextBtn.addEventListener("click", () => {
+        counter++;
+        carousel();
+    });
 
-//     prevBtn.addEventListener("click", () => {
-//         counter--;
-//         carousel();
-//     });
+    prevBtn.addEventListener("click", () => {
+        counter--;
+        carousel();
+    });
 
-//     function carousel() {
-//         if (counter > 0) {
-//             prevBtn.style.display = "block";
-//         } else {
-//             prevBtn.style.display = "none";
-//         }
-//         if (counter < slide.length - 1) {
-//             nextBtn.style.display = "block";
-//         } else {
-//             nextBtn.style.display = "none";
-//         }
-//         slides.style.transform = `translateX(-${100 * counter}vw)`;
-//     }
-// });
+    function carousel() {
+        if (counter > 0) {
+            prevBtn.style.display = "block";
+        } else {
+            prevBtn.style.display = "none";
+        }
+        if (counter < slide.length - 1) {
+            nextBtn.style.display = "block";
+        } else {
+            nextBtn.style.display = "none";
+        }
+        slides.style.transform = `translateX(-${100 * counter}vw)`;
+    }
+});
 
 const prevbtn = document.querySelector("#prevbtn");
 const nextbtn = document.querySelector("#nextbtn");
